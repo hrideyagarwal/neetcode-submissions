f4554d52a67ab3faf464de8d43f63a2483ebce6a
Hridey Agarwal <67671401+hrideyagarwal@users.noreply.github.com>
2026-08-07 23:45:02 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        if (s1.size() > s2.size())
+            return false;
+
+        vector<int> need(26, 0);
+        vector<int> window(26, 0);
+
+        for (char c : s1)
+            need[c - 'a']++;
+
+        int k = s1.size();
+
+        for (int i = 0; i < k; i++)
+            window[s2[i] - 'a']++;
+
+        if (window == need)
+            return true;
+
+        for (int i = k; i < s2.size(); i++) {
+
+            window[s2[i] - 'a']++;
+            window[s2[i - k] - 'a']--;
+
+            if (window == need)
+                return true;
+        }
+
+        return false;
+    }
+};
